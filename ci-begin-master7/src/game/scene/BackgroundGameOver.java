b8fb374bae45c0177dff6d2ee9df355036dd6c18
nhudo <nhudo267@gmail.com>
2018-12-23 20:45:24 +0700

    mm

--- ci-begin-master7/src/game/scene/BackgroundGameOver.java
+++ ci-begin-master7/src/game/scene/BackgroundGameOver.java
@@ -6,25 +6,18 @@ import game.renderer.SingleImageRenderer;
 import tklibs.SpriteUtils;
 
 public class BackgroundGameOver extends GameObject {
-        FrameCounter frame ;
-
-
+    
     public BackgroundGameOver(){
         this.renderer = new SingleImageRenderer(SpriteUtils.loadImage("assets/images/background/na.jpg"));
         this.position.set(Settings.SCREEN_WIDTH/2,Settings.SCREEN_HEIGHT/2);
-        this.frame = new FrameCounter(10);
+
     }
 
     @Override
     public void run() {
-
-        if (this.frame.run()){
-
             if (GameWindow.isAnyKeyPress){
                 SceneManager.signNewScene(new SceneWelcome());
-                this.frame.reset();
-
             }
-        }
+
     }
 }
